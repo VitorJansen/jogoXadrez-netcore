2c3b35363103e86ec9cc4fe9563a3eff62c30561
Vitor Jansen <vitorvieira.id@gmail.com>
2021-02-10 16:42:34 -0300

    Melhorias na tela para imprimir movimentos permitidos

--- jogoXadrez-netcore/Program.cs
+++ jogoXadrez-netcore/Program.cs
@@ -21,6 +21,12 @@ namespace jogoXadrez_netcore
 
                     Console.Write("Origem: ");
                     Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+
+                    bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPosiveis();
+
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+
                     Console.Write("Destino: ");
                     Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
 
