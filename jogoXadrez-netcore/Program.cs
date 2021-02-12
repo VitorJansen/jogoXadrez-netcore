d8f0f30784c769f561f162bf4b7dbddc397aca0b
Vitor Jansen <vitorvieira.id@gmail.com>
2021-02-12 12:02:24 -0300

    Implementacao do sistema de controle de captura

--- jogoXadrez-netcore/Program.cs
+++ jogoXadrez-netcore/Program.cs
@@ -19,10 +19,7 @@ namespace jogoXadrez_netcore
                     try
                     {
                         Console.Clear();
-                        Tela.imprimirTabuleiro(partida.tab);
-                        Console.WriteLine();
-                        Console.WriteLine("Turno: " + partida.turno);
-                        Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
+                        Tela.imprimirPartida(partida);
 
                         Console.Write("Origem: ");
                         Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
